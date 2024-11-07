be68c0f477ec0c10c28971f01f5c25e1e6c479cd
WinandriSihotang <8040210263@student.unama.ac.id>
2024-11-07 17:27:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+function printHeart() {
+    console.log("<3");
+  }
+  printHeart();
+
+  function multiply(a, b) {
+    return a * b;
+  }
+  console.log(multiply(2, 3)); 
+  console.log(multiply(9, 9)); 
+  console.log(multiply(5, 4)); 
+
+
+let animal = "Giant Pacific Octopus";
+
+  function observe() {
+    let animal = "Pajama Squid";
+    console.log(animal);
+  }
+  
+  observe();
+
+  let deadlyAnimal = "Blue-Ringed Octopus";
+
+  function handleAnimal() {
+    let deadlyAnimal = "Scorpionfish";
+    console.log(deadlyAnimal);
+  }
+  
+  handleAnimal();
+  console.log(deadlyAnimal);
+
+  
+  const sayHello = (name) => {
+    return `Hello ${name}!`;
+  };
+  
+  // Test cases
+  console.log(sayHello("Hagrid")); // "Hello Hagrid!"
+  console.log(sayHello("Luna"));   // "Hello Luna!"
+  
+  
+  
+  
